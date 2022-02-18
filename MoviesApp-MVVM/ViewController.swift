aa866eaa1df279c29c9b9642bc1287618d33ccb9
ugurhmz <craxx67@gmail.com>
2022-02-18 03:36:33 +0300

    first

---
+++ MoviesApp-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp-MVVM
+//
+//  Created by ugur-pc on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
