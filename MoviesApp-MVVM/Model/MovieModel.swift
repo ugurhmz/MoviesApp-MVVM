bc4bae5540d49fdaf8d201d63fe5f4f381c68e50
ugurhmz <craxx67@gmail.com>
2022-02-18 04:46:43 +0300

    MovieModel created

---
+++ MoviesApp-MVVM/Model/MovieModel.swift
@@ -0,0 +1,51 @@
+
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let page: Int?
+    let results: [Result]?
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages
+        case totalResults
+    }
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIDS: [Int]?
+    let id: Int?
+    let originalLanguage: OriginalLanguage?
+    let originalTitle, overview: String?
+    let popularity: Double?
+    let posterPath, releaseDate, title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath
+        case genreIDS
+        case id
+        case originalLanguage
+        case originalTitle
+        case overview, popularity
+        case posterPath
+        case releaseDate
+        case title, video
+        case voteAverage
+        case voteCount
+    }
+}
+
+enum OriginalLanguage: String, Codable {
+    case en = "en"
+    case es = "es"
+    case hi = "hi"
+}
